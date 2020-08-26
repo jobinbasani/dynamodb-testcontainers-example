83887f25255e8209e04bafc548c7fb86d225e718
Jobin Basani <jobinbasani@gmail.com>
2020-08-25 21:20:04 -0300

    Update testcases

--- src/test/java/com/jobinbasani/service/DatabaseServiceImplTest.java
+++ src/test/java/com/jobinbasani/service/DatabaseServiceImplTest.java
@@ -4,9 +4,7 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.*;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -17,12 +15,15 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 @Testcontainers
 @TestInstance(PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DatabaseServiceImplTest {
 
     @Container
     static GenericContainer dynamoDb = new GenericContainer("amazon/dynamodb-local:1.13.2")
+            .withCommand("-jar DynamoDBLocal.jar -inMemory -sharedDb")
             .withExposedPorts(8000);
     DatabaseService databaseService;
+    String tableName;
 
     @BeforeAll
     public void init() {
@@ -30,18 +31,21 @@ class DatabaseServiceImplTest {
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:" + dynamoDb.getFirstMappedPort(), "us-west-2"))
                 .build();
         databaseService = new DatabaseServiceImpl(client);
+        tableName = "TestTable";
     }
 
     @Test
+    @Order(1)
     void createTable() throws InterruptedException {
-        databaseService.createTable("TestTable", List.of(new KeySchemaElement("year", KeyType.HASH)),
+        databaseService.createTable(tableName, List.of(new KeySchemaElement("year", KeyType.HASH)),
                 List.of(new AttributeDefinition("year", ScalarAttributeType.S)),
                 new ProvisionedThroughput(1L, 1L));
 
     }
 
     @Test
-    void deleteTable() {
-        System.out.println(dynamoDb.getContainerId());
+    @Order(2)
+    void deleteTable() throws InterruptedException {
+        databaseService.deleteTable(tableName);
     }
 }
